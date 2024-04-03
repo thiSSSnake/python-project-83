1b31633d60839ad148fa7a0fc5ac6134dcff75d2
Andrei Emelianenko <spliitvein@gmail.com>
2024-04-04 00:10:28 +0300

    add index.html to templates

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,13 +1,18 @@
-from flask import Flask
+from flask import Flask, request, render_template, redirect, url_for, flash, get_flashed_messages
 import os
 from dotenv import load_dotenv
+
 
 load_dotenv()
 SECRET = os.getenv('SECRET_KEY')
 
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = SECRET
 
+
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def index():
+    return render_template(
+        'index.html'
+    )
